dc481b65adcc64a9d043d9a6ed75e3f523169cc6
Dimitris Papoutsakis <dimitrispapoutsakis@gmail.com>
2025-07-11 01:06:24 +0300

    fix: Linting.

--- awesome-cursor/rslib.config.ts
+++ awesome-cursor/rslib.config.ts
@@ -1,61 +1,61 @@
-import { pluginReact } from "@rsbuild/plugin-react";
-import { pluginSass } from "@rsbuild/plugin-sass";
-import { defineConfig } from "@rslib/core";
-import { pluginModuleFederation } from "@module-federation/rsbuild-plugin";
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginSass } from '@rsbuild/plugin-sass';
+import { defineConfig } from '@rslib/core';
 
 export default defineConfig({
-	lib: [
-		{
-			bundle: false,
-			dts: true,
-			format: "esm",
-		},
-		{
-			format: "mf",
-			output: {
-				distPath: {
-					root: "./dist/mf",
-				},
-				// for production, add online assetPrefix here
-				assetPrefix: "http://localhost:3001/mf",
-			},
-			// for Storybook to dev
-			dev: {
-				assetPrefix: "http://localhost:3001/mf",
-			},
-			plugins: [
-				pluginModuleFederation(
-					{
-						name: "awesome-cursor",
-						exposes: {
-							".": "./src/index.tsx",
-						},
-						// can not add 'remote' here, because you may build 'esm' or 'cjs' assets in one build.
-						// if you want the Rslib package use remote module, please see below.
-						shared: {
-							react: {
-								eager: true,
-								singleton: true,
-							},
-							"react-dom": {
-								eager: true,
-								singleton: true,
-							},
-						},
-					},
-					{
-						ssr: false,
-						environment: "development",
-					},
-				),
-			],
-		},
-	],
-	server: {
-		port: 3001,
-	},
-	output: {
-		target: "web",
-	},
-	plugins: [pluginReact(), pluginSass()],
+  lib: [
+    {
+      bundle: false,
+      dts: true,
+      format: 'esm',
+    },
+    {
+      format: 'mf',
+      output: {
+        distPath: {
+          root: './dist/mf',
+        },
+        // for production, add online assetPrefix here
+        assetPrefix: 'http://localhost:3001/mf',
+      },
+      // for Storybook to dev
+      dev: {
+        assetPrefix: 'http://localhost:3001/mf',
+      },
+      plugins: [
+        pluginModuleFederation(
+          {
+            name: 'awesome-cursor',
+            exposes: {
+              '.': './src/index.tsx',
+            },
+            // can not add 'remote' here, because you may build 'esm' or 'cjs' assets in one build.
+            // if you want the Rslib package use remote module, please see below.
+            shared: {
+              react: {
+                eager: true,
+                singleton: true,
+              },
+              'react-dom': {
+                eager: true,
+                singleton: true,
+              },
+            },
+          },
+          {
+            ssr: false,
+            environment: 'development',
+          },
+        ),
+      ],
+    },
+  ],
+  server: {
+    port: 3001,
+  },
+  output: {
+    target: 'web',
+  },
+  plugins: [pluginReact(), pluginSass()],
 });
--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,16 +1,16 @@
-import "./App.css";
+import './App.css';
 // import { AwesomeCursor } from "awesome-cursor";
 
 const App = () => {
-	return (
-		<>
-			{/* <AwesomeCursor /> */}
-			<div className="content">
-				<h1>Rsbuild with React</h1>
-				<p>Start building amazing things with Rsbuild.</p>
-			</div>
-		</>
-	);
+  return (
+    <>
+      {/* <AwesomeCursor /> */}
+      <div className="content">
+        <h1>Rsbuild with React</h1>
+        <p>Start building amazing things with Rsbuild.</p>
+      </div>
+    </>
+  );
 };
 
 export default App;
