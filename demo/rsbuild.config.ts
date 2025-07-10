d1363a67945179e87bb45fd0064074c89bfb968f
Dimitris Papoutsakis <dimitrispapoutsakis@gmail.com>
2025-07-11 01:05:06 +0300

    feat: Initial infra.

---
+++ demo/rsbuild.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginModuleFederation } from "@module-federation/rsbuild-plugin";
+
+export default defineConfig({
+	plugins: [
+		pluginReact(),
+		pluginModuleFederation(
+			{
+				name: "demo",
+				remotes: {
+					reactuploadform:
+						"awesome-cursor@http://localhost:3001/mf/mf-manifest.json",
+				},
+				shared: {
+					react: {
+						eager: true,
+						singleton: true,
+					},
+					"react-dom": {
+						eager: true,
+						singleton: true,
+					},
+				},
+			},
+			{
+				ssr: false,
+				environment: "development",
+			},
+		),
+	],
+});
